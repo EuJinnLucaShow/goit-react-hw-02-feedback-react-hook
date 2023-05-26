aae3182fcd68e0f36c280e8895844312acc1c881
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-26 18:51:58 +0300

    react hook

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification'
+import { AppContainer } from './App.styled'
+
+function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleButtonClick = (type) => {
+    setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <AppContainer>
+      <Section title="Review Widget 📝">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleButtonClick}
+        />
+      </Section>
+
+      <Section title="Statistics 📊">
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    </AppContainer>
+  );
+}
+
+export default App;
+
